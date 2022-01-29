c62e7484eec7ffa9018e03aad094bfa80bbb25ea
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-29 17:51:34 -0600

    finished rough draft layout html

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,19 @@
 
 
 // API fetch and function
+const weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1cea6050e017bee6a2d6d8c3cc614e1e";
 
+var getWeatherApi = function () {
+    fetch(weatherApi)
+    .then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+            })
+        }
+    })
+};
+
+getWeatherApi();
 
 // save to localStorage
