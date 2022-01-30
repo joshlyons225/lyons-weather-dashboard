911cb2469862bdb362e687a58bbf9d390eb6d5d5
joshlyons225 <95392565+joshlyons225@users.noreply.github.com>
2022-01-30 11:24:48 -0600

    fixed getApi function to include text value typed into form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 // globals
-
+var searchButtonEl = document.querySelector("#searchBtn");
 
 // API fetch and function
-const weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=city,country&APPID=1cea6050e017bee6a2d6d8c3cc614e1e";
-
 var getWeatherApi = function () {
+    var cityName = document.getElementById('city-search').value;
+    console.log(cityName);
+    const weatherApi = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=1cea6050e017bee6a2d6d8c3cc614e1e";
     fetch(weatherApi)
     .then(function(response) {
         if (response.ok) {
@@ -15,6 +16,10 @@ var getWeatherApi = function () {
     })
 };
 
-getWeatherApi();
-
 // save to localStorage
+
+// eventListener for Search button
+searchButtonEl.addEventListener("click", getWeatherApi);
+
+
+
